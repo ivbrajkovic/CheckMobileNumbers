eb6cff23299ee8300697f8838b43752ec9a30607
Ivan Brajković <44271953+ivbrajkovic@users.noreply.github.com>
2020-03-19 19:13:58 +0100

    add heroku config

--- client/src/components/Table/index.js
+++ client/src/components/Table/index.js
@@ -1,15 +1,15 @@
 // Table
 
-import { h } from 'preact';
+import { h } from './node_modules/preact';
 
 // Material-UI
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableContainer from '@material-ui/core/TableContainer';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
+import Table from './node_modules/@material-ui/core/table';
+import TableBody from './node_modules/@material-ui/core/TableBody';
+import TableCell from './node_modules/@material-ui/core/TableCell';
+import TableContainer from './node_modules/@material-ui/core/TableContainer';
+import TableHead from './node_modules/@material-ui/core/TableHead';
+import TableRow from './node_modules/@material-ui/core/TableRow';
+import Paper from './node_modules/@material-ui/core/Paper';
 
 // Custom styles
 import useStyles from './style';
